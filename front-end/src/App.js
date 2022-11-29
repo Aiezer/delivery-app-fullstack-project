11160da462ee2d485c7f7bf4e66118b0646c3a09
Tiago Eberhardt Flor <tiagoeberhardtflor1996@gmail.com>
2022-11-29 16:27:47 -0400

    feat: adcionando pagina de login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact path="/login" component={ Login } />
+    </Switch>
   );
 }
 
---
+++ front-end/src/pages/Login.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import loginRequest from '../utils/request';
+
+const six = 6;
+
+export default function Login() {
+  const history = useHistory();
+  const [user, setUser] = useState({ email: '', password: '' });
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  // função do email e password verificação
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+
+    const VALIDATE_EMAIL = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (user.password.length > six
+      && (VALIDATE_EMAIL.test(user.email))
+    ) return setIsDisabled(false);
+
+    if (user.password.length < six
+      || !(VALIDATE_EMAIL.test(user.email))
+    ) return setIsDisabled(true);
+  };
+
+  function handleClick() {
+    const data = loginRequest(user);
+    localStorage.setItem('user', JSON.stringify(user));
+    // localStorage.setItem('userId', JSON.stringify({ userId: id }));
+    
+  }
+
+  return (
+    <section>
+      <div>
+        <img src='' />
+        <h1>NOME DO APP</h1>
+      </div>
+      <div>
+        <label htmlFor='email'>
+          Email
+          <input
+            id="email"
+            type="text"
+            name="email"
+            value={ user.email }
+            placeholder="Email"
+            onChange={ handleChange }
+          />
+        </label>
+        <label htmlFor='password' >
+          Password
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={ user.email }
+            placeholder="Email"
+            onChange={ handleChange }
+          />
+        </label>
+        <label htmlFor='login'>
+          Login
+          <button
+            id='login'
+            name='enter'
+            type='button'
+            disabled={ isDisabled }
+            onClick={ handleClick }
+          />
+        </label>
+        <label htmlFor='register'>
+          Register
+          <button
+            id='register'
+            name='register'
+            type='button'
+            onClick={ () => history.push("/register") }
+          />
+        </label>
+      </div>
+    </section>
+  )
+}
