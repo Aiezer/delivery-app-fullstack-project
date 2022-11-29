490c6d107d759246ffe992e0333e8845eaadec51
Thales <thales.souz@outlook.com>
2022-11-29 18:22:42 -0300

    feat: rota para "/customer" implementada

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,12 @@
+const CustomerRouter = require('./Routes');
+const { json } = require('body-parser');
 const express = require('express');
 
 const app = express();
 
+app.use(express.json())
+
+app.use('/customer', CustomerRouter)
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
