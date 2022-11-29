9b2ce818ba02580fb63ed2770ff69f36bf07c48d
Thales <thales.souz@outlook.com>
2022-11-29 18:21:56 -0300

    feat: controller de customer

---
+++ back-end/src/api/Controllers/Customer.controller.js
@@ -0,0 +1,11 @@
+const customerService = require('../Services/Customer.service');
+
+const register = async (req, res) => {
+  const newUser = await customerService.register(req.body);
+
+  return res.status(201).json(newUser);
+}
+
+module.exports = {
+  register
+}
