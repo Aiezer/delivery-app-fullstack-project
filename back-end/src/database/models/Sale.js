4d5b28bf1fe4ca9af6539558a86199397c7674b2
Thales <thales.souz@outlook.com>
2022-11-29 18:35:51 -0300

    fix: corrigido erro nas models

--- back-end/src/database/models/Sale.js
+++ back-end/src/database/models/Sale.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
   );
   
   Sales.associate = (models) => {
-    blogPostSchema.belongsTo(models.User, {
+    Sales.belongsTo(models.user, {
       as: 'user', 
       foreignKey: 'userId' 
     })
