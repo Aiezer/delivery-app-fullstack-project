5dc03f65c70a4308e6b3aab0b04bce5531cc3376
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-11-29 18:12:02 -0300

    adiciona primeiros testes da rota login

---
+++ back-end/src/tests/Mocks.js
@@ -0,0 +1,3 @@
+export const tokenMock = 'Adicionar token mock'
+
+export const loginMock = 'Adiciona Mock do retorno do Banco'
---
+++ back-end/src/tests/login.test.js
@@ -0,0 +1,43 @@
+const { expect } = require('chai')
+const sinon = require('sinon')
+const app = require('../api/app')
+const chaiHttp = require('chai-http')
+const { Model } = require('sequelize')
+const { describe } = require('pm2')
+const JsonWebToken = require('jsonwebtoken')
+const { tokenMock, loginMock } = require('./Mocks')
+
+chai.use(chaiHttp)
+
+describe('testa a rota /login', function () {
+  it('testa se é possível realizar um login como consumidor com sucesso', async function() {
+    before(async () => {
+      sinon.stub(Model, 'findOne').resolves(loginMock);
+      sinon.stub(JsonWebToken, 'sign').resolves(tokenMock)
+
+    })
+
+    const response = await chai.request(app).post('/login').send({
+     email: 'zebirita@email.com',
+     senha: '$#zebirita#$',
+    })
+
+    expect(response.status).to.be.equal(200)
+    expect(response.body).to.be.deep.equal({token: tokenMock })
+  })
+
+  it('testa que não é possível fazer login com dados inexistentes no banco', async function () {
+    before(async () => {
+        sinon.stub(Model, 'findOne').resolves()
+    })
+    const response = await chai.request(app).post('/login').send({
+        email: 'inexistente@email.com',
+        senha: 'inexistente',
+       })
+   
+       expect(response.status).to.be.equal(404)
+       expect(response.body).to.be.deep.equal({message: 'Not found' })
+    })
+
+  afterEach(sinon.restore);
+})
