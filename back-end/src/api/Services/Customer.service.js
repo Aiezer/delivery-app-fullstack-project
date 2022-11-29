92571a9ef20339c381d4b4eff1905913c255b9f7
Thales <thales.souz@outlook.com>
2022-11-29 18:54:00 -0300

    feat: rota para cadastro de um novo cliente

--- back-end/src/api/Services/Customer.service.js
+++ back-end/src/api/Services/Customer.service.js
@@ -1,5 +1,14 @@
-const register = (body) => {
-  
+const { user } = require('../../database/models')
+const md5 = require('md5');
+
+const register = async (body) => {
+  const hashPassword = md5(body.password);
+
+  const newUser = await user.create({
+    ...body,
+    password: hashPassword,
+  });
+  return newUser;
 }
 
 module.exports = {
