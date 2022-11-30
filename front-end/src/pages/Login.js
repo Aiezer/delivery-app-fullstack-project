b7c5fd3272568f7015ffd1fd702083c1976ca831
Tiago Eberhardt Flor <tiagoeberhardtflor1996@gmail.com>
2022-11-30 14:07:21 -0400

    feat: atualizando branch

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -9,7 +9,6 @@ export default function Login() {
   const [user, setUser] = useState({ email: '', password: '' });
   const [isDisabled, setIsDisabled] = useState(true);
 
-  // função do email e password verificação
   const handleChange = (e) => {
     const { name, value } = e.target;
 
@@ -29,44 +28,49 @@ export default function Login() {
     ) return setIsDisabled(true);
   };
 
-  function handleClick() {
-    const data = loginRequest(user);
+  async function handleClick() {
+    const data = await loginRequest(user);
     localStorage.setItem('user', JSON.stringify(user));
-    history.push(`/${data.role}/products`)
+    if (data.role === "seller") history.push(`/${data.role}/orders`)
+    if (data.role === "costumer") history.push(`/${data.role}/products`)
+    if (data.role === "admin") history.push(`/${data.role}/manage`)
   }
 
   return (
     <section>
       <div>
         <img src='' />
-        <h1>NOME DO APP</h1>
+        <h1>GENERICS DELIVERY</h1>
       </div>
       <div>
         <label htmlFor='email'>
-          Email
+          Login
           <input
+            datatestid="common_login__input-email"
             id="email"
             type="text"
             name="email"
             value={ user.email }
-            placeholder="Email"
+            placeholder="generics@delivery.com.br"
             onChange={ handleChange }
           />
         </label>
         <label htmlFor='password' >
-          Password
+          Senha
           <input
+            datatestid="common_login__input-password"
             id="password"
             type="password"
             name="password"
             value={ user.password }
-            placeholder="Password"
+            placeholder="***********"
             onChange={ handleChange }
           />
         </label>
         <label htmlFor='login'>
-          Login
+          LOGIN
           <button
+            datatestid="common_login__button-login"
             id='login'
             name='enter'
             type='button'
@@ -75,8 +79,9 @@ export default function Login() {
           />
         </label>
         <label htmlFor='register'>
-          Register
+          Ainda não tenho conta
           <button
+            datatestid="common_login__button-login"
             id='register'
             name='register'
             type='button'
