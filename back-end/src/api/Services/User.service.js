752dade8b7609f02a57a3d9e4ec7b0a482c3545c
Luiz Gabriel <lgabrielpessoal@gmail.com>
2022-11-30 15:00:12 -0300

    Lint correction

--- back-end/src/api/Services/User.service.js
+++ back-end/src/api/Services/User.service.js
@@ -3,11 +3,11 @@ const { user } = require('../../database/models');
 
 const login = async (email, password) => {
   const findUser = await user.findAll({ where: { email } });
-  if (!findUser[0]) return { bool: false, message: "Não existe esse usuário!" };
+  if (!findUser[0]) return { bool: false, message: 'Não existe esse usuário!' };
   const passwordHash = md5(password);
-  if(passwordHash === findUser[0].password) return { bool: true, message: findUser[0] };
-  return { bool: false, message: "A senha está incorreta!" };
-}
+  if (passwordHash === findUser[0].password) return { bool: true, message: findUser[0] };
+  return { bool: false, message: 'A senha está incorreta!' };
+};
 
 const register = async (body) => {
   const hashPassword = md5(body.password);
