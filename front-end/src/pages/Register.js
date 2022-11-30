f250361cca0b8e4390c9690ed582a75aeb538131
Thales <thales.souz@outlook.com>
2022-11-30 16:14:39 -0300

    feat: form de cadastro estrutura base finalizada

--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Register() {
+  const [error, setError] = useState(false);
+
+  const handleSubmit = () => {
+    setError(true);
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={ handleSubmit }>
         <h2>Cadastro</h2>
         <label htmlFor="input-name">
           <h3>Nome</h3>
-          <input type="text" placeholder="Seu nome" id="input-name" />
+          <input
+            type="text"
+            placeholder="Seu nome"
+            id="input-name"
+            data-testid="common_register_input-name"
+          />
         </label>
         <label htmlFor="input-email">
           <h3>Email</h3>
-          <input type="email" placeholder="Seu nome" id="input-email" />
+          <input
+            type="email"
+            placeholder="seu-email@site.com.br"
+            id="input-email"
+            data-testid="common_register_input-name"
+          />
         </label>
-        <label htmlFor="input-email">
-          <h3>Email</h3>
-          <input type="email" placeholder="Seu nome" id="input-email" />
+        <label htmlFor="input-senha">
+          <h3>Senha</h3>
+          <input
+            type="password"
+            placeholder="*******"
+            id="input-senha"
+            data-testid="common_register_input-name"
+          />
         </label>
-        <button type="submit">CADASTRAR</button>
+        <div>
+          <button
+            type="submit"
+            data-testid="common_register_input-name"
+          >
+            CADASTRAR
+          </button>
+        </div>
       </form>
+      {error ? <span data-testid="common_register_input-name">Error</span> : null }
     </div>
   );
 }
