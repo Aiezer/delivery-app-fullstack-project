78d4978b05932d6faa36a7fd26af149d10dec704
Luiz Gabriel <lgabrielpessoal@gmail.com>
2022-12-01 14:23:34 -0300

    GET /products feito (controller, service + route)

--- back-end/src/api/Routes/index.js
+++ back-end/src/api/Routes/index.js
@@ -1,9 +1,11 @@
 const UserRouter = require('./User.routes');
 const ValidateRouter = require('./Validate.routes');
 const AdminRouter = require('./Admin.routes');
+const ProductRouter = require('./Product.routes');
 
 module.exports = {
   UserRouter,
   AdminRouter,
-  ValidateRouter
+  ValidateRouter,
+  ProductRouter
 };
--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const errorHandler = require('./Middlewares/error.middleware');
-const { UserRouter, ValidateRouter, AdminRouter } = require('./Routes');
+const { UserRouter, ValidateRouter, AdminRouter, ProductRouter } = require('./Routes');
 
 const app = express();
 
@@ -10,6 +10,7 @@ app.use('/customer', UserRouter);
 app.use('/login', UserRouter);
 app.use('/admin', AdminRouter);
 app.use('/validate', ValidateRouter);
+app.use('/products', ProductRouter);
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use(errorHandler);
