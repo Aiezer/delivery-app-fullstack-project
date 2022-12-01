0574777933f04ee0ee073c5fae48ab2c5f29d00d
Thales <thales.souz@outlook.com>
2022-12-01 15:17:32 -0300

    fix: corrigido validação do Joi

--- back-end/src/api/utils/Schemas.js
+++ back-end/src/api/utils/Schemas.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const registreSchema = Joi.object({
-  name: Joi.string().max(12).required(),
+  name: Joi.string().min(12).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
   role: Joi.string(),
