53cf9273f19d9fdeb211a2645262640722bb117c
Thales <thales.souz@outlook.com>
2022-12-07 16:53:07 -0300

    feat: implementado lógica para identificar carrinho vazio

--- front-end/src/components/GenericRowCheckout.jsx
+++ front-end/src/components/GenericRowCheckout.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 
-function GenericRowCheckout(product) {
-  const { id, name, quantity, value } = product;
-  const total = quantity * value;
+function GenericRowCheckout(product, index) {
+  const { name, quantity, price } = product;
+  const total = quantity * price;
 
   const handleClick = () => {
 
   };
 
   return (
-    <tr data-testid={ `customer_checkout__element-order-table-item-number-${id}` }>
-      <td data-testid={ `customer_checkout__element-order-table-item-number-${id}` }>
-        {id}
+    <tr data-testid={ `element-order-table-item-number-${index}` }>
+      <td data-testid={ `customer_checkout__element-order-table-item-number-${index}` }>
+        {index}
       </td>
-      <td data-testid={ `customer_checkout__element-order-table-item-number-${id}` }>
+      <td data-testid={ `customer_checkout__element-order-table-name-${index}` }>
         {name}
       </td>
-      <td data-testid={ `customer_checkout__element-order-table-item-number-${id}` }>
+      <td data-testid={ `customer_checkout__element-order-table-quantity-${index}` }>
         {quantity}
       </td>
-      <td data-testid={ `customer_checkout__element-order-table-item-number-${id}` }>
-        {value}
+      <td data-testid={ `customer_checkout__element-order-table-unity-price-${index}` }>
+        {price}
       </td>
-      <td data-testid={ `customer_checkout__element-order-table-item-number-${id}` }>
+      <td data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }>
         {total}
       </td>
-      <td data-testid={ `customer_checkout__element-order-table-item-number-${id}` }>
+      <td data-testid={ `customer_checkout__element-order-table-remove-${index}` }>
         <button type="button" onClick={ handleClick }>Remover</button>
       </td>
     </tr>
--- front-end/src/components/ProductsCheckout/index.js
+++ front-end/src/components/ProductsCheckout/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import GenericRowCheckout from '../GenericRowCheckout';
 import './index.css';
 
 function ProductsCheckout() {
+  const [cart, setCart] = useState();
+
+  useEffect(() => {
+    const carrinho = localStorage.getItem('carrinho');
+    if (carrinho) {
+      return setCart(carrinho);
+    }
+  }, []);
+
   return (
     <section>
       Finalizar Pedido
@@ -15,8 +25,22 @@ function ProductsCheckout() {
             <th>Sub-total</th>
             <th>Remover Item</th>
           </tr>
+          { cart ? (
+            cart.map((cartItem, i) => (
+              <GenericRowCheckout
+                products={ cartItem }
+                index={ i }
+                key={ i }
+              />))
+          ) : (
+            <tr>
+              <td>Carrinho Vazio!</td>
+            </tr>
+          )}
         </table>
-        <div>Total:</div>
+        <div data-testid="customer_checkout__elemente-order-total-price">
+          {`Total: ${cart}`}
+        </div>
       </div>
     </section>
   );
