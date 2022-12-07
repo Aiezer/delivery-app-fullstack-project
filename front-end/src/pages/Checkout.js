b0fb5f922073d393e3400e8b246fe0fdaefb5ecc
Thales <thales.souz@outlook.com>
2022-12-07 15:01:20 -0300

    build: iniciando pagina de checkout do cliente

---
+++ front-end/src/pages/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavbarProducts from '../components/NavbarProducts';
+
+function Checkout() {
+  return (
+    <section>
+      <NavbarProducts />
+    </section>
+  );
+}
+
+export default Checkout;
