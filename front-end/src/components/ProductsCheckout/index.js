907426cbfbce4377cb09e7c3eed4b4e3a238b472
Thales <thales.souz@outlook.com>
2022-12-07 15:24:34 -0300

    build: página de checkout - componente para visualizar o carrinho

---
+++ front-end/src/components/ProductsCheckout/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './index.css';
+
+function ProductsCheckout() {
+  return (
+    <section>
+      Finalizar Pedido
+      <div className="table-container">
+        <table>
+          <tr>
+            <th>Item</th>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Valor Unitário</th>
+            <th>Sub-total</th>
+            <th>Remover Item</th>
+          </tr>
+
+        </table>
+        <div>Total:</div>
+      </div>
+    </section>
+  );
+}
+
+export default ProductsCheckout;
--- front-end/src/pages/Checkout.js
+++ front-end/src/pages/Checkout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavbarProducts from '../components/NavbarProducts';
+import ProductsCheckout from '../components/ProductsCheckout';
 
 function Checkout() {
   return (
     <section>
       <NavbarProducts />
+      <ProductsCheckout />
     </section>
   );
 }
