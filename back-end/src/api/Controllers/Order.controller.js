52973c57bb9bd9191aab7fa6034dd88797b2e30f
Luiz Gabriel <lgabrielpessoal@gmail.com>
2022-12-07 18:36:01 -0300

    Alteração na requisição GET /orders para filtrar as orders

--- back-end/src/api/Controllers/Order.controller.js
+++ back-end/src/api/Controllers/Order.controller.js
@@ -1,8 +1,10 @@
 const orderService = require('../Services/Order.service');
 
-const getOrders = async (_req, res) => {
+const getOrders = async (req, res) => {
   try {
-    const orders = await orderService.getOrders();
+    const token = req.header('Authorization')
+    const orders = await orderService.getOrders(token);
+    if (orders === false) return res.status(200).json({ message: "Você não tem pedidos!" });
     return res.status(200).json(orders);
   } catch (e) {
     console.log(e.error);
--- back-end/src/api/Services/Order.service.js
+++ back-end/src/api/Services/Order.service.js
@@ -1,12 +1,13 @@
-const { sale } = require('../../database/models');
+const { sale, user } = require('../../database/models');
 const jwt = require('jsonwebtoken');
 
-const secret = process.env.JWT_SECRET || 'jwt_secret';
+const secret = require('fs')
+  .readFileSync('../back-end/jwt.evaluation.key', { encoding: 'utf-8' });
 
 const getOrders = async (token) => {
     const decoded = jwt.verify(token, secret);
-    // const email = 
-    const findOrders = await sale.findAll();
+    const userFind = await user.findAll({ where: { email: decoded.email } })
+    const findOrders = await sale.findAll({ where: { userId: userFind[0].id } });
     if (!findOrders[0]) return false;
     return findOrders;
 };
