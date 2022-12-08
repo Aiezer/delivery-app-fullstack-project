13190e54db0bc6cf4e0d1b9e206f9ff14a64406b
Thales <thales.souz@outlook.com>
2022-12-08 14:44:01 -0300

    feat: rota de sale criada

--- back-end/src/api/Routes/index.js
+++ back-end/src/api/Routes/index.js
@@ -2,10 +2,12 @@ const UserRouter = require('./User.routes');
 const ValidateRouter = require('./Validate.routes');
 const AdminRouter = require('./Admin.routes');
 const ProductRouter = require('./Product.routes');
+const SaleRouter = require('./Sale.routes');
 
 module.exports = {
   UserRouter,
   AdminRouter,
   ValidateRouter,
   ProductRouter,
+  SaleRouter,
 };
