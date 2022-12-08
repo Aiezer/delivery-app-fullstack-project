f8e044a5680a3f9bca0f49756f280d86404ee617
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-12-08 17:05:38 -0300

    corrige erros fluxo admin

--- front-end/src/pages/Admin.js
+++ front-end/src/pages/Admin.js
@@ -28,41 +28,30 @@ function Admin() {
   };
 
   const verify = async (token) => {
-    // const header = { 'Autorization': `${token}` };
     const validation = await axios.post('http://localhost:3001/validate', {
     }, {
       headers: { Authorization: token },
-    }).then((result) => {
-      console.log('debtro do then ', result);
-      return result.data;
-    }).catch((err) => {
+    }).then((result) => result.data).catch((err) => {
       if (err) {
         setError(true);
       }
     });
-    console.log('dentro do validate', validation);
     return validation;
   };
 
   const handleClick = async () => {
     const user = JSON.parse(localStorage.getItem('user'));
-    console.log('user do local', user);
 
     const validToken = await verify(user.token);
-    console.log('valid token', validToken);
 
     if (validToken) {
       await axios.post('http://localhost:3001/admin/register', { ...form })
-        .then((response) => {
-          console.log('debtro do then 2 ', response);
+        .then(() => {
+          setForm({
+            name: '', email: '', password: '', role: 'cliente' });
           setError(false);
         })
-        .catch((err) => {
-          if (err) {
-            console.log(err);
-            setError(true);
-          }
-        });
+        .catch(() => setError(true));
     }
   };
 
