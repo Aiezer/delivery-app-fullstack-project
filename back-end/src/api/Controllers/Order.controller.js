1fa16fb5d2302e9ec97108ab9e9774ff4f9546b4
Aiezer <aiezer.santos01@gmail.com>
2022-12-12 18:35:34 -0300

    feat: refatorando o service da rota /orders.

--- back-end/src/api/Controllers/Order.controller.js
+++ back-end/src/api/Controllers/Order.controller.js
@@ -2,10 +2,10 @@ const orderService = require('../Services/Order.service');
 
 const getOrders = async (req, res) => {
   try {
-    const token = req.header('Authorization')
+    const token = req.header('Authorization');
     const { type } = req.body;
     const orders = await orderService.getOrders(token, type);
-    if (orders === false) return res.status(200).json({ message: "Você não tem pedidos!" });
+    if (orders === false) return res.status(200).json({ message: 'Você não tem pedidos!' });
     return res.status(200).json(orders);
   } catch (e) {
     console.log(e.error);
