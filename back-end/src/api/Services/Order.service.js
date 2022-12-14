6ef8819ec60e4a1efae7c05aa76f36abee62c5a5
Luiz Gabriel <lgabrielpessoal@gmail.com>
2022-12-14 17:22:28 -0300

    Requisito 23

--- back-end/src/api/Services/Order.service.js
+++ back-end/src/api/Services/Order.service.js
@@ -1,29 +1,29 @@
 const { sale, product } = require('../../database/models');
 
+const getObjectSale = (sales) => {
+  const { id, userId, sellerId, totalPrice, deliveryAddress, 
+    deliveryNumber, saleDate, status, products } = sales;
+    const format = saleDate.toLocaleString();
+    const finalDate = format.split(' ')[0];
+    return {
+      id,
+      userId,
+      sellerId,
+      totalPrice: totalPrice.replace('.', ','),
+      deliveryAddress,
+      deliveryNumber,
+      saleDate: finalDate,
+      status,
+      products,
+    }; 
+};
+
   const getAll = async (userId) => {
     const customerOrders = await sale.findAll({
       where: { userId },
     });
-
-    return customerOrders;
-  };
-
-  const getObjectSale = (sales) => {
-    const { id, userId, sellerId, totalPrice, deliveryAddress, 
-      deliveryNumber, saleDate, status, products } = sales;
-      const format = saleDate.toLocaleString();
-      const finalDate = format.split(' ')[0];
-      return {
-        id,
-        userId,
-        sellerId,
-        totalPrice,
-        deliveryAddress,
-        deliveryNumber,
-        saleDate: finalDate,
-        status,
-        products,
-      }; 
+    const mapOrders = customerOrders.map((order) => getObjectSale(order))
+    return mapOrders;
   };
 
   const getBySaleId = async (userId, saleId, role) => {
