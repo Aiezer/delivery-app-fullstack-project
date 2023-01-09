b460ebc878463f765dbce42fb6c0f90f6e733adb
Luiz Gabriel <lgabrielpessoal@gmail.com>
2023-01-09 16:26:31 -0300

    CSS Navbar e Products

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 import { getProducts } from '../utils/request';
 
 export default function ProductCard() {
@@ -63,53 +66,75 @@ export default function ProductCard() {
 
   return (
     <>
-      {products.map((p, index) => (
-        <div key={ index }>
-          <p data-testid={ `customer_products__element-card-price-${p.id}` }>
-            {`${((p.price).toString()).replace('.', ',')}`}
+      <div className="flex justify-center items-center mt-10">
+        <button
+          type="button"
+          data-testid="customer_products__button-cart"
+          onClick={ () => navigate('/customer/checkout') }
+          disabled={ isDisable }
+          className="bg-[#DCE91C] p-4 rounded-xl"
+        >
+          <p data-testid="customer_products__checkout-bottom-value">
+            <ShoppingCartCheckoutIcon />
+            {`R$${total.replace('.', ',')}`}
           </p>
-          <img
-            alt={ `foto de ${p.name}` }
-            src={ p.urlImage }
-            data-testid={ `customer_products__img-card-bg-image-${p.id}` }
-          />
-          <p data-testid={ `customer_products__element-card-title-${p.id}` }>{p.name}</p>
-          <div>
-            <button
-              data-testid={ `customer_products__button-card-rm-item-${p.id}` }
-              type="button"
-              onClick={ () => subtractQuantity(p.id) }
+        </button>
+      </div>
+      <div className="flex flex-wrap m-10 justify-center">
+        {products.map((p, index) => (
+          <div
+            key={ index }
+            className="m-6 p-6 bg-[#DCE91C] rounded-xl shadow-lg shadow-[#919191]"
+          >
+            <p
+              data-testid={ `customer_products__element-card-price-${p.id}` }
+              className="font-bold mb-2"
             >
-              -
-            </button>
-            <input
-              data-testid={ `customer_products__input-card-quantity-${p.id}` }
-              id={ `customer_products__input-card-quantity-${p.id}` }
-              type="number"
-              onChange={ () => setChange(change + 1) }
-              defaultValue={ 0 }
-            />
-            <button
-              data-testid={ `customer_products__button-card-add-item-${p.id}` }
-              type="button"
-              onClick={ () => sumQuantity(p.id) }
-            >
-              +
-            </button>
+              {`R$${((p.price).toString()).replace('.', ',')}`}
+            </p>
+            <div className="flex flex-col items-center justify-center">
+              <img
+                alt={ `foto de ${p.name}` }
+                width="250"
+                src={ p.urlImage }
+                data-testid={ `customer_products__img-card-bg-image-${p.id}` }
+              />
+              <p
+                data-testid={ `customer_products__element-card-title-${p.id}` }
+                className="mt-2 mb-2 font-light"
+              >
+                {p.name}
+              </p>
+              <div className="flex w-18 mt-2 items-center">
+                <button
+                  data-testid={ `customer_products__button-card-rm-item-${p.id}` }
+                  type="button"
+                  onClick={ () => subtractQuantity(p.id) }
+                  className="bg-[#EC2C31] rounded-xl mr-2"
+                >
+                  <RemoveIcon sx={ { color: 'white' } } />
+                </button>
+                <input
+                  data-testid={ `customer_products__input-card-quantity-${p.id}` }
+                  id={ `customer_products__input-card-quantity-${p.id}` }
+                  type="number"
+                  onChange={ () => setChange(change + 1) }
+                  defaultValue={ 0 }
+                  className="w-6"
+                />
+                <button
+                  data-testid={ `customer_products__button-card-add-item-${p.id}` }
+                  type="button"
+                  onClick={ () => sumQuantity(p.id) }
+                  className="bg-[#EC2C31] rounded-xl ml-2"
+                >
+                  <AddIcon sx={ { color: 'white' } } />
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
-      <button
-        type="button"
-        data-testid="customer_products__button-cart"
-        onClick={ () => navigate('/customer/checkout') }
-        disabled={ isDisable }
-      >
-        Ver carrinho:
-        <p data-testid="customer_products__checkout-bottom-value">
-          {`${total.replace('.', ',')}`}
-        </p>
-      </button>
+        ))}
+      </div>
     </>
   );
 }
